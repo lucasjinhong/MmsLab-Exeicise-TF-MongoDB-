f3601e394329f5fb3d6bf7b4218f9cd696a4e37f
lucasjinhong <55587534+lucasjinhong@users.noreply.github.com>
2022-01-15 22:22:27 +0800

    mongoDB

---
+++ MongoDB/Lab10/Lab11/Lab11/Models/MembersCollection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace Lab11.Models
+{
+    public class MembersCollection
+    {
+        public ObjectId _id { get; set; }
+        public string uid { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+    }
+}
